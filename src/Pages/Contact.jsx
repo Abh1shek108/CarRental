50fd183fd79a507b7cf110173395a82e48a38c0f
Abh1shek108 <151020626+Abh1shek108@users.noreply.github.com>
2023-11-30 17:07:16 +0530

    Contact.jsx

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -25,7 +25,7 @@ function Contact() {
               </a>
               <a href="/">
                 <IconLocation />
-                &nbsp; Belgrade, Serbia
+                &nbsp; Beanglore
               </a>
             </div>
             <div className="contact-div__form">
